7b007ede9d406d426b7a4a0854ea54aae1db753d
romario2299 <romario2299@gmail.com>
2020-05-02 19:27:51 -0500

    Servidor en Heroku

---
+++ config.js
@@ -0,0 +1,17 @@
+//Variables de entorno
+
+//Puerto del servidor
+process.env.PORT = process.env.PORT || 3000;
+
+//Configuración y autenticación en Firestore
+process.env.APIKEY = process.env.APIKEY;
+process.env.AUTHDOMAIN = process.env.AUTHDOMAIN;
+process.env.DATABASEURL = process.env.DATABASEURL; 
+process.env.PROJECTID = process.env.PROJECTID;
+process.env.STORAGEBUCKET = process.env.STORAGEBUCKET;
+process.env.MESSAGINGSENDERID = process.env.MESSAGINGSENDERID;
+process.env.APPID = process.env.APPID;
+
+//Autenticación en Instagram
+process.env.ACCESS_TOKEN = process.env.ACCESS_TOKEN;
+process.env.APP_SECRET = process.env.APP_SECRET;
---
+++ server.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const app = express();
+const axios = require('axios').default;
+const firebase = require('firebase');
+const bodyParser = require('body-parser');
+
+access_token = process.env.ACCESS_TOKEN;
+app_secret = process.env.APP_SECRET;
+
+firebaseConfig = {
+    apiKey: process.env.APIKEY,
+    authDomain: process.env.AUTHDOMAIN,
+    databaseURL: process.env.DATABASEURL,
+    projectId: process.env.PROJECTID,
+    storageBucket: process.env.STORAGEBUCKET,
+    messagingSenderId: process.env.MESSAGINGSENDERID,
+    appId: process.env.APPID
+  };
+firebase.initializeApp(firebaseConfig);
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
+
+
+app.get('/instagram', (req, resp) => {
+    axios.get(`https://graph.instagram.com/access_token?grant_type=ig_exchange_token&client_secret=${app_secret}&access_token=${access_token}`)
+    .then( (response) => {
+        access_token = response.data.access_token;
+        resp.json({auth: access_token});
+    })
+    .catch( (error) => {
+        resp.status(401).json({
+            ok: false,
+            message : 'Mensaje de error',
+            error: error
+        });
+    }); 
+});
+
+app.post('/firestore', (req, resp) => {
+    const db = firebase.firestore();
+    usuario = req.body;
+    console.log(usuario);
+    let result = db.collection('bienvenido').add({
+        name: usuario.nombre,
+        email: usuario.email,
+        mensaje: usuario.mensaje
+      });
+    resp.send(result);
+});
+
+app.listen(3000, () => {
+    console.log('servidor corriendo');
+});
